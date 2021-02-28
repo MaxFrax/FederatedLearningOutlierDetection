e23de456ec417f88aa1e3c2386c16cd57765f160
Massimo Frasson <maxfrax@gmail.com>
2021-02-28 15:57:20 +0100

    Adds support to cache folder

--- flod/__main__.py
+++ flod/__main__.py
@@ -1,2 +1,20 @@
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log", mode='w'),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger(__name__)
+CACHE_FOLDER = os.path.abspath('./cache/')
+
 if __name__ == '__main__':
-    print('Hello world')
+    if not os.path.exists(CACHE_FOLDER):
+        LOGGER.info(f'Creating CACHE folder at {CACHE_FOLDER}')
+        os.makedirs(CACHE_FOLDER)
+    
