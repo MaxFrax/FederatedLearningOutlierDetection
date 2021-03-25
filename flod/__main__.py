584432a81c195e703c8acd8d30114cb06372ff83
Massimo Frasson <maxfrax@gmail.com>
2021-03-25 21:18:27 +0100

    A lot of code that should do stuff, but doesn't

--- flod/__main__.py
+++ flod/__main__.py
@@ -3,9 +3,12 @@ import logging
 from flod.dataset import download_dataset
 from flod.features_extraction.load_features import load_features
 import json
-from sklearn.model_selection import KFold, cross_validate
+from sklearn.model_selection import KFold, cross_validate, train_test_split, RandomizedSearchCV
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
 from flod.classifiers.bsvclassifier import BSVClassifier
 import numpy as np
+from scipy.stats import uniform as sp_randFloat
+from scipy.stats import randint as sp_randInt
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -31,8 +34,42 @@ if __name__ == '__main__':
     X = np.array(dataset[['c1', 'c2', 'c3', 'c4']])  # Features
     y = np.array(dataset['is_fall'])  # Labels
 
-    clf = BSVClassifier(random_state=42)
+    clf = BSVClassifier(c=0.03142918568673425, penalization=230, q=51)
 
-    cv = KFold(n_splits=10, shuffle=True, random_state=42)
-    
-    print(cross_validate(clf, X, y, cv=cv, scoring=['accuracy', 'precision', 'recall', 'f1']))
+    # Split dataset into training set and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=100, test_size=200, stratify=y, shuffle=True)
+
+    LOGGER.info(f'Training size {len(X_train)} and test size {len(X_test)}. Outliers in training {sum(y_train)}')
+
+    clf.fit(X_train, y_train)
+
+    LOGGER.info(f'Sum betas {sum(clf.betas_)} Count neg {len([x for x in clf.betas_ if x < 0])}')
+
+    y_pred = clf.predict(X_test)
+
+    LOGGER.info(f'Predicted. Outliers in test sample are {sum(y_test)}')
+
+    print(confusion_matrix(y_test, y_pred))
+
+    print(f'Precision {precision_score(y_test, y_pred)}')
+    print(f'Recall {recall_score(y_test, y_pred)}')
+    print(f'F1 {f1_score(y_test, y_pred)}')
+
+
+    parameters = {
+        'c': sp_randFloat(0, 1),
+        'q': sp_randInt(0, 200),
+        'penalization' : sp_randInt(0, 400),
+        'n_iter': [10]
+    }
+
+    #Come viene calcolato lo score?
+    model = BSVClassifier()
+    randm_src = RandomizedSearchCV(estimator=model, param_distributions = parameters,
+                               cv = 2, n_iter = 5, n_jobs=-1)
+    randm_src.fit(X_train, y_train)
+
+    print(" Results from Random Search " )
+    print("\n The best estimator across ALL searched params:\n", randm_src.best_estimator_)
+    print("\n The best score across ALL searched params:\n", randm_src.best_score_)
+    print("\n The best parameters across ALL searched params:\n", randm_src.best_params_)
--- flod/classifiers/bsvclassifier.py
+++ flod/classifiers/bsvclassifier.py
@@ -1,10 +1,24 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 import random
+from typing import Iterable
+import logging
+import tensorflow as tf
+import numpy as np 
+from sklearn.metrics import accuracy_score
+
+LOGGER = logging.getLogger(__name__)
 
 class BSVClassifier(ClassifierMixin, BaseEstimator):
 
-    def __init__(self, random_state:int):
-        self.random_state = random_state
+    def __init__(self, c: float = 0.25, q:float = 1, random_seed:int = 42, init_bound:float = .1, n_iter:int = 100, penalization: int = 10):
+        self.random_seed = random_seed
+        self.init_bound = init_bound
+        self.q = q
+        self.c = c
+        self.n_iter = n_iter
+        # Should the optimizer be a parameter?
+        self.optimizer = tf.optimizers.Adam(learning_rate=1e-4)
+        self.penalization = penalization
 
     def fit(self, X, y):
 
@@ -12,15 +26,134 @@ class BSVClassifier(ClassifierMixin, BaseEstimator):
         
         self.X_ = X
         self.y_ = y
+
+        # Suggested c value for big datasets
+        #self.c_ = 1 / (sum(y) * len(y)) if sum(y) > 0 else 1
+
+        LOGGER.debug(f'Solving optimization {len(X)} with c {self.c}')
+
+        self.betas_, self.constant_term_ = BSVClassifier._solve_optimization_tensorflow(X, y, self.random_seed, self.init_bound, self.c, self.q, self.n_iter, self.optimizer, self.penalization)
+
+        self.radiuses_ = [self._compute_r(x) for x in X]
+
+        self.sv_i, self.sv_ = self._best_precision_sv()        
+
         # Return the classifier
         return self
 
     def predict(self, X):
-        
-        # TODO verificare che la funzione fit sia stata chiamata, altrimenti dare errore
-
         # Input validation
         #X = check_array(X)
+        
+        if self.betas_ is None or self.radiuses_ is None:
+            LOGGER.error('You must call fit before predict!')
 
-        random.seed(a=self.random_state, version=2)
-        return [random.choice([0, 1]) for _ in range(len(X))]
+        rs = [self._compute_r(x) for x in X]
+        prediction = [int(ri > self.radiuses_[self.sv_i]) for ri in rs]
+
+        return prediction
+
+    @staticmethod
+    def _gaussian_kernel(x_i: Iterable, x_j: Iterable, q: float) -> float:
+        squared_norm = np.linalg.norm(np.array(x_i) - np.array(x_j)) ** 2
+        return np.float64(np.exp(-1 * q * squared_norm))
+
+    @staticmethod
+    def _solve_optimization_tensorflow(xs, y, random_seed, init_bound, c, q, n_iter, optimizer, penalization):
+
+        if abs(init_bound) > abs(c):
+            LOGGER.warning(f'init_bound cannot be bigger than c. Defaulting to init_bound=c')
+            init_bound = c
+        
+        betas = []
+        
+        LOGGER.debug(f'Optimizing with q {q}')
+        gaussian_kernel = lambda x1, x2: BSVClassifier._gaussian_kernel(x1, x2, q)
+
+        # Inits beta values randomly
+        np.random.seed(random_seed)
+        init_values = np.random.uniform(0, init_bound, len(xs))
+        for i in range(len(xs)):
+            # Ho fissato le variabili legate agli outliers. Sono che hanno beta=C. Perchè farle cambiare?
+            init = c if y[i] == 1 else init_values[i]
+            beta = tf.Variable(init, name=f'beta_{i}', trainable=True, dtype=tf.float64)
+            betas.append(beta)
+
+        self_kernels = [gaussian_kernel(x_i, x_i) for x_i in xs]
+        kernel_values = np.array([[gaussian_kernel(x1, x2) for x1 in xs] for x2 in xs])
+        kernels = tf.constant(kernel_values, dtype='float64')
+
+        def objective_function():
+            constant_term = tf.tensordot(tf.linalg.matvec(kernels, betas), betas, axes=1)
+
+            # The wolfe equation is the dual problem for the lagrangian.
+            # This means we have to maximize the function.
+            # Here i multiply everything for -1 so that I can still minimize
+            v = constant_term
+            v += tf.tensordot(betas, self_kernels, axes=1)
+            
+            # Penalize if sum of betas is not close to 1 from the bottom
+            v += penalization * tf.math.maximum(0, 1 - sum(betas))
+            # Penalize if the sum of the betas goes above 1
+            v += penalization * tf.math.maximum(0, sum(betas) - 1)
+
+            # Penalize for each beta below zero
+            v += -penalization * sum([x for x in betas if x < 0])
+
+            # Penalize for each beta that goes above C
+            error_c = sum([(b-c) for b in betas if b - c > 0])
+            #error_c = sum([(b-c) / c for b in betas if b - c > 0])
+            v += penalization * error_c
+
+            return v
+
+        best_v = objective_function()
+        best_betas = [b.numpy() for b in betas]
+        i = 0
+        tot_iter = 0
+        last_update = 0
+        emergency_limit = 5000
+
+        while i < n_iter and tot_iter < emergency_limit:
+            optimizer.minimize(objective_function, betas)
+            v = objective_function()
+
+            i += 1
+            tot_iter += 1
+
+            if v < best_v:
+                best_betas = [b.numpy() for b in betas]
+                i = 0
+                last_update = best_v - v
+                best_v = v
+
+        LOGGER.info(f'Optimized in {tot_iter} iterations. Latest update size: {last_update}')
+
+        constant_term = tf.tensordot(tf.linalg.matvec(kernels, best_betas), best_betas, axes=1)
+
+        return best_betas, constant_term
+
+    def _compute_r(self, x) -> float:
+        v = self.constant_term_
+        v += BSVClassifier._gaussian_kernel(x,x, self.q)
+        v += -2 * tf.tensordot(self.betas_, [self._gaussian_kernel(x_i, x, self.q) for x_i in self.X_], axes=1)
+        v = np.sqrt(v)
+        return v
+
+    def _best_precision_sv(self):
+        score = 0
+        best_i = 0
+        sv = None
+
+        for i, x in enumerate(self.X_):
+            r = self.radiuses_[i]
+            
+            prediction = [int(ri > r) for ri in self.radiuses_]
+            s = accuracy_score(self.y_, prediction)
+            
+            if s > score:
+                score = s
+                best_i = i
+                sv = x
+
+        return best_i, sv
