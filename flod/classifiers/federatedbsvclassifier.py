1e7708a9081da3fe12dfd8c0aed0e3076263ff5f
Massimo <maxfrax@gmail.com>
2022-11-01 11:13:07 +0000

    FederatedLearningBSV first fit iteration

--- flod/classifiers/federatedbsvclassifier.py
+++ flod/classifiers/federatedbsvclassifier.py
@@ -1,35 +1,71 @@
 import numpy as np
 from ..classifiers.bsvclassifier import BSVClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.base import BaseEstimator, ClassifierMixin
+from collections import defaultdict
+from scipy.stats import uniform
 
-def count_zeros(y, y_pred, **kwargs):
-    return len(y)-sum(y_pred)
+# Reference paper: Communication-Efficient Learning of Deep Network from Decentralized Data
 
-scoring = {
-    'zeros_scorer': make_scorer(count_zeros)
-}
+class FederatedBSVClassifier(ClassifierMixin, BaseEstimator):
 
-class FederatedBSVClassifier():
+    def __init__(self, client_fraction:float = 1, batch_size:int = -1, total_clients:int = 10, max_rounds: int = 10, normal_class_label:int=0, outlier_class_label:int=1):
+        self.normal_class_label = normal_class_label
+        self.outlier_class_label = outlier_class_label
+        self.client_fraction = client_fraction # Known as C in the reference paper
+        self.batch_size = batch_size # Known as B in the reference paper
+        self.total_clients = total_clients # Known as K in the paper
+        self.max_rounds = max_rounds
 
-    def __init__(self, grid_search_parameters):
-        self.grid_search_parameters = grid_search_parameters
+    def __getstate__(self) -> dict:
+        state = self.__dict__
+        return state
 
-    def init_server_model(self):
+    def __setstate__(self, state):
+        self.__dict__ = state
+
+    def init_server_model(self, dimensions_count):
+        # TODO is it a good idea to keep this fixed and not random? Should initial state be an hyper parameter?
         return {
             'q': 0.01,
-            'C': 1,
-            'betas': np.empty(shape=(0, 2)),
-            'xs': np.empty(shape=(0, 2))
+            'c': 1,
+            'betas': np.empty(shape=(0, )),
+            'xs': np.empty(shape=(0, dimensions_count))
     }
 
+    def fit(self, X, y, client_assignment):
+
+        clients_x = defaultdict(list)
+
+        for i, x in enumerate(X):
+            assignment = client_assignment[i]
+            clients_x[assignment].append(x)
+
+        model = self.init_server_model(X.shape[1])
+
+        for _ in range(self.max_rounds):
+            selected_clients_count = max(1, self.total_clients * self.client_fraction)
+            clients_ix = np.random.choice(range(self.total_clients), int(selected_clients_count))
+
+            updates = []
+
+            for c_ix in clients_ix:
+                updates.append(self.client_compute_update(model, clients_x[c_ix]))
+
+            model, clf = self.global_combine(model, updates)
+
+        return self
+
+    def predict(self, X):
+        return np.random.choice([self.normal_class_label, self.outlier_class_label], len(X))
 
     def client_compute_update(self, global_model, client_data):
+        # TODO add support for batch size B, like in Algorithm 1 of the paper
         # Concat points from server and from client
         X = np.concatenate((global_model['xs'], client_data))
 
         # Init the classifier with q and C from server
-        clf = BSVClassifier(q=global_model['q'], c=global_model['C'])
+        clf = BSVClassifier(q=global_model['q'], c=global_model['c'])
 
         # Train locally
         clf.fit(X, [0]*len(X))
@@ -37,29 +73,54 @@ class FederatedBSVClassifier():
         # Select only the positive betas related from client_data
         client_betas = clf.betas_[len(global_model['xs']):]
         assert(len(client_betas) == len(client_data))
+
+        xs = []
+        betas = []
         
         for _, t in enumerate(zip(client_betas, client_data)):
             b, x = t
             if not np.isclose(b, 0):
-                yield x
+                xs.append(x)
+                betas.append(b)
+
+        if len(xs) == 0:
+            print('There is no client update. No betas far from zero')
+        
+        return np.array(xs), np.array(betas)
 
     def global_combine(self, global_model, client_updates):
         # Concatenates server points and clients candidate points.
-        X = np.concatenate((global_model['xs'], *client_updates))
-        
+        X = global_model['xs']
+        betas = global_model['betas']
+        for update in client_updates:
+            xs, bs = update
+            if xs.shape[0] == 0:
+                break
+            X = np.concatenate((X, xs))
+            betas = np.concatenate((betas, bs))
+
+        # TODO che senso ha prendere i beta e compararli con global c quando loro stessi si sono trovati una c?
+        #y = np.array([self.outlier_class_label if np.isclose(b, global_model['c']) else self.normal_class_label for b in betas])
+        y = np.array([self.normal_class_label for _ in range(len(X))])
+        # TODO: we should get also the betas from the client. When a we receive a BSV, b = global C, we should give it y = outlier. The others have y = inlier
+
         # Let's try also old hyper parameters. Notice: C is missing
-        search_params = dict(self.grid_search_parameters)
-        search_params['q'] = np.append(search_params['q'], global_model['q'])
+        search_params = {
+            'q': [global_model['q']],
+            'c': [global_model['c']]
+        }
+        search_params['q'].extend(uniform(loc=0, scale=1).rvs(size=10))
+        search_params['c'].extend(uniform(loc=.2, scale=.8).rvs(size=10))
         
         # Performs model selection over this new dataset
-        # Cross validation is low because I want to fit exactly the data I have got
-        clf = GridSearchCV(BSVClassifier(), search_params, cv=2, n_jobs=1, scoring=scoring, refit='zeros_scorer', return_train_score=False, verbose=10)
-        clf.fit(X, [0] * len(X))
+        test_fold = [0 if v < len(X) else 1 for v in range(len(X) * 2)]
+        clf = GridSearchCV(BSVClassifier(outlier_class_label=self.outlier_class_label, normal_class_label=self.normal_class_label), search_params, cv=PredefinedSplit(test_fold=test_fold), n_jobs=1, scoring='completeness_score', refit=True, return_train_score=False, error_score='raise', verbose=1)
+        clf.fit(np.concatenate((X,X)), np.concatenate((y,y)))
         
         # Filter and keep only the support vectors
         xs = []
         betas = []
-        for i, t in enumerate(zip(clf.best_estimator_.betas_, X)):
+        for _, t in enumerate(zip(clf.best_estimator_.betas_, X)):
             b, x = t
             if not np.isclose(b, 0):
                 xs.append(x)
@@ -67,7 +128,15 @@ class FederatedBSVClassifier():
                 
         return {
             'q': clf.best_estimator_.q,
-            'C': clf.best_estimator_.c,
-            'betas': betas,
-            'xs': xs
+            'c': clf.best_estimator_.c,
+            'betas': np.array(betas),
+            'xs': np.array(xs)
         }, clf.best_estimator_
+
+    def decision_function(self, X):
+        # TODO what is the federated learning decision function? Probably the same as bsvclassifier
+        return np.random.rand(len(X))
+
+    def score_samples(self, X):
+        # TODO what is the federated learning decision function? Probably the same as bsvclassifier
+        return np.random.rand(len(X))
