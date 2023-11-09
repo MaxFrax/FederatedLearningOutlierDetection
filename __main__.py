eb485019a7c3fbbfecc63853f8a60e42caf2e815
Massimo <maxfrax@gmail.com>
2023-11-09 10:33:07 +0000

    Removes duplicate code in baseline computation

--- __main__.py
+++ __main__.py
@@ -4,22 +4,10 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import argparse
 import logging
 import sys
-import numpy as np
-import pandas as pd
-import os
 from sklearn.svm import OneClassSVM
 from scipy.stats import uniform
-from sklearn.preprocessing import MinMaxScaler
 from flod.classifiers.bsvclassifier import BSVClassifier
-from experiments.baseline import svm_experiment
-import glob
-
-def get_datasets():
-    datasets = {}
-    for dataset in glob.glob('./datasets/*.csv'):
-        datasets[os.path.splitext(os.path.basename(dataset))[0].replace('-unsupervised-ad','')] = dataset
-
-    return datasets
+from experiments.baseline import compute_baseline, get_datasets
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -43,57 +31,14 @@ parser.add_argument('dataset', type=str, choices=get_datasets().keys(), help='Th
 # parse the arguments
 args = parser.parse_args()
 
-
-def get_dataset_from_path(path):
-
-    data = pd.read_csv(path, header=0)
-    data = data.sample(frac=1)
-
-    data.columns = [f'x{i}' for i in range(len(data.columns) - 1)] + ['outlier']
-
-    X = data[data.columns[:-1]]
-    y = data['outlier'] == 'o'
-
-    y = [-1 if v else 1 for v in y]
-
-    X = MinMaxScaler().fit_transform(X)
-
-    return X, y
-
 def baseline_sklearn():
-    auc_res_path = 'sklearn_auc.csv'
-    avg_res_path = 'sklearn_avg.csv'
-
     logger.info('Running baseline sklearn experiment on %s', args.dataset)
 
     classifier = OneClassSVM(kernel='rbf')
     distributions = dict(nu=uniform(loc=0.2, scale=0.8),
                          gamma=uniform(loc=0, scale=3))
     
-    try:
-        auc_df = pd.read_csv(auc_res_path, index_col=0)
-    except FileNotFoundError:
-        auc_df = pd.DataFrame()
-
-    try:
-        avg_df = pd.read_csv(avg_res_path, index_col=0)
-    except FileNotFoundError:
-        avg_df = pd.DataFrame()
-
-    dinfo = get_datasets()[args.dataset]
-
-    X,y = get_dataset_from_path(dinfo)
-    res = svm_experiment(X, y, classifier, distributions, args.njobs)
-
-    auc_df[args.dataset] = {
-        classifier.__class__.__name__: f"{res['roc_auc']['mean']:.4f} ± {res['roc_auc']['std']:.4f}"
-    }
-    avg_df[args.dataset] = {
-        classifier.__class__.__name__: f"{res['average_precision']['mean']:.4f} ± {res['average_precision']['std']:.4f}"
-    }
-
-    auc_df.to_csv('sklearn_auc.csv')
-    avg_df.to_csv('sklearn_avg.csv')
+    auc_df, avg_df = compute_baseline('sklearn_auc.csv', 'sklearn_avg.csv', classifier, distributions, args.dataset, args.njobs)
 
     print('AUC')
     print(auc_df)
@@ -101,44 +46,17 @@ def baseline_sklearn():
     print(avg_df)
 
 def baseline_svdd():
-    auc_res_path = 'svdd_auc.csv'
-    avg_res_path = 'svdd_avg.csv'
-
     logger.info('Running baseline sklearn experiment on %s', args.dataset)
 
     classifier = BSVClassifier(normal_class_label=1, outlier_class_label=-1)
     distributions = {'c':uniform(loc=0.2, scale=0.8),'q':uniform(loc=0, scale=3)}
     
-    try:
-        auc_df = pd.read_csv(auc_res_path, index_col=0)
-    except FileNotFoundError:
-        auc_df = pd.DataFrame()
-
-    try:
-        avg_df = pd.read_csv(avg_res_path, index_col=0)
-    except FileNotFoundError:
-        avg_df = pd.DataFrame()
-
-    dinfo = get_datasets()[args.dataset]
-
-    X,y = get_dataset_from_path(dinfo)
-    res = svm_experiment(X, y, classifier, distributions, args.njobs)
-
-    auc_df[args.dataset] = {
-        classifier.__class__.__name__: f"{res['roc_auc']['mean']:.4f} ± {res['roc_auc']['std']:.4f}"
-    }
-    avg_df[args.dataset] = {
-        classifier.__class__.__name__: f"{res['average_precision']['mean']:.4f} ± {res['average_precision']['std']:.4f}"
-    }
-
-    auc_df.to_csv('sklearn_auc.csv')
-    avg_df.to_csv('sklearn_avg.csv')
+    auc_df, avg_df = compute_baseline('svdd_auc.csv', 'svdd_avg.csv', classifier, distributions, args.dataset, args.njobs)
 
     print('AUC')
     print(auc_df)
     print('Average Precision')
     print(avg_df)
-
 
 if args.loglevel:
     logger.setLevel(args.loglevel)
--- experiments/baseline.py
+++ experiments/baseline.py
@@ -1,6 +1,9 @@
+import glob
+import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import PredefinedSplit, RandomizedSearchCV
+from sklearn.preprocessing import MinMaxScaler
 
 def svm_experiment(X: np.ndarray, y: np.array, classifier, distributions, njobs: int) -> (float, float):
     test_fold = [0 if v < len(X) else 1 for v in range(len(X) * 2)]
@@ -22,3 +25,54 @@ def svm_experiment(X: np.ndarray, y: np.array, classifier, distributions, njobs:
             'std': np.std(cv_results['mean_test_average_precision'])
         }
     }
+
+def compute_baseline(auc_res_path, avg_res_path, classifier, distributions, dataset, njobs):
+    try:
+        auc_df = pd.read_csv(auc_res_path, index_col=0)
+    except FileNotFoundError:
+        auc_df = pd.DataFrame()
+
+    try:
+        avg_df = pd.read_csv(avg_res_path, index_col=0)
+    except FileNotFoundError:
+        avg_df = pd.DataFrame()
+
+    dinfo = get_datasets()[dataset]
+
+    X,y = get_dataset_from_path(dinfo)
+    res = svm_experiment(X, y, classifier, distributions, njobs)
+
+    auc_df[dataset] = {
+        classifier.__class__.__name__: f"{res['roc_auc']['mean']:.4f} ± {res['roc_auc']['std']:.4f}"
+    }
+    avg_df[dataset] = {
+        classifier.__class__.__name__: f"{res['average_precision']['mean']:.4f} ± {res['average_precision']['std']:.4f}"
+    }
+
+    auc_df.to_csv(auc_res_path)
+    avg_df.to_csv(avg_res_path)
+
+    return auc_df, avg_df
+
+def get_dataset_from_path(path):
+
+    data = pd.read_csv(path, header=0)
+    data = data.sample(frac=1)
+
+    data.columns = [f'x{i}' for i in range(len(data.columns) - 1)] + ['outlier']
+
+    X = data[data.columns[:-1]]
+    y = data['outlier'] == 'o'
+
+    y = [-1 if v else 1 for v in y]
+
+    X = MinMaxScaler().fit_transform(X)
+
+    return X, y
+
+def get_datasets():
+    datasets = {}
+    for dataset in glob.glob('./datasets/*.csv'):
+        datasets[os.path.splitext(os.path.basename(dataset))[0].replace('-unsupervised-ad','')] = dataset
+
+    return datasets
