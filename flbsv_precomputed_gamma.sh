3a2c62cc43e7095f0bb3a965ac1769b92f3af948
Massimo <maxfrax@gmail.com>
2023-11-28 15:13:35 +0000

    Adds datasets to the flbsv experiments script

--- flbsv_precomputed_gamma.sh
+++ flbsv_precomputed_gamma.sh
@@ -3,3 +3,29 @@ start=$(date +%s)
 python __main__.py flbsv_precomputed_gamma pen-global --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py flbsv_precomputed_gamma breast-cancer --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py flbsv_precomputed_gamma letter --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py flbsv_precomputed_gamma pen-local --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py flbsv_precomputed_gamma annthyroid --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo "WARNING: this is not a full evaluation. Some datasets missing"
