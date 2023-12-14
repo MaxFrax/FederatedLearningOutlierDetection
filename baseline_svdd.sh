66397f1765f36dbdac393c68d07b800766fd74d1
Massimo <maxfrax@gmail.com>
2023-12-14 19:31:02 +0100

    Reduces the number of concurrent jobs for baseline_svdd

--- baseline_svdd.sh
+++ baseline_svdd.sh
@@ -1,53 +1,53 @@
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd pen-global --njobs 6
+python __main__.py dp_flbsv pen-global --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd breast-cancer --njobs 6
+python __main__.py dp_flbsv breast-cancer --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd letter --njobs 6
+python __main__.py dp_flbsv letter --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd pen-local --njobs 6
+python __main__.py dp_flbsv pen-local --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd annthyroid --njobs 6
+python __main__.py dp_flbsv annthyroid --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd satellite --njobs 1
+python __main__.py dp_flbsv satellite --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd shuttle --njobs 1
+python __main__.py dp_flbsv shuttle --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd aloi --njobs 1
+python __main__.py dp_flbsv aloi --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
 
 echo $(date)
 start=$(date +%s)
-python __main__.py baseline_svdd kdd99 --njobs 1
+python __main__.py dp_flbsv kdd99 --njobs 1
 end=$(date +%s)
 echo "Elapsed time: $(($end-$start)) s"
