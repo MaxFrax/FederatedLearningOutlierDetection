e81fee65674f8e5cad3f6778b9bc9a8a684dca73
Massimo <maxfrax@gmail.com>
2023-12-19 10:03:20 +0100

    Adds script to launch dpflbsv noisy on two clients

---
+++ iid_dpflbsv_2_noisy.sh
@@ -0,0 +1,107 @@
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy pen-global 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy pen-global 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy breast-cancer 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy breast-cancer 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy letter 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy letter 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy pen-local 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy pen-local 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy annthyroid 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy annthyroid 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy satellite 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy satellite 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy shuttle 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy shuttle 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy aloi 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy aloi 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy kdd99 2 1 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
+
+echo $(date)
+start=$(date +%s)
+python __main__.py dp_flbsv_noisy kdd99 2 .5 True --njobs 1
+end=$(date +%s)
+echo "Elapsed time: $(($end-$start)) s"
