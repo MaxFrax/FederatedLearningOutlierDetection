cef67096f90fe78da0bb03780eb77d1104d4d88a
Massimo <maxfrax@gmail.com>
2023-12-20 11:31:38 +0100

    Fixes client assignment generation in the non iid case

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -91,7 +91,8 @@ def compute_federated_experiment(classifier, distributions, dataset, njobs, iid,
             assignment = np.load(f'non_iid_assignment_{dataset}_{clients}.npy', allow_pickle=True)
         except FileNotFoundError:
             LOGGER.warning(f'Could not find non iid assignment file for {clients} clients. Generating a new one.')
-            KMeans(n_clusters=clients).fit_predict(X).dump(f'non_iid_assignment_{dataset}_{clients}.npy')
+            assignment = KMeans(n_clusters=clients).fit_predict(X)
+            assignment.dump(f'non_iid_assignment_{dataset}_{clients}.npy')
 
     fit_params = {
         'client_assignment': assignment,
